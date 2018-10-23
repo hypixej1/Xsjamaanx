066e196ac43c1c6aec4326cce8bc54665a9aac7b
hypixej <40128873+hypixej@users.noreply.github.com>
2018-10-24 00:30:10 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -24,12 +24,6 @@ client.on('message', message => {
 });
 
 client.on('message', message => {
-    if (message.content === 'forgot that was a command') {
-    	message.reply('shut the fuck up!');
-  	}
-});
-
-client.on('message', message => {
     if (message.content === 'Xsjamaanx is bad') {
     	message.reply('Dont talk to my brother like that! >:(');
   	}
